ab355f68ebbd251be1cfe711e60e80c62bd6a1b2
Shinde Rahul <shinde2732000@gmail.com>
2023-08-18 10:37:50 +0530

    added new feature for user registration and getting all user

--- 3JwtAuthentication/src/server.ts
+++ 3JwtAuthentication/src/server.ts
@@ -1,1 +1,18 @@
-console.log("setting");
+// importing confidential data from config
+import { config } from "./config/config";
+import express from "express";
+import authRouter from "./routes/auth";
+const app = express();
+const port = config.port as string;
+
+app.use(express.json());
+app.use("/auth", authRouter);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+// start the server
+app.listen(port, () => {
+  // make a connection to the database
+  config.db();
+  console.log(`Example app listening on port ${port}!`);
+});
